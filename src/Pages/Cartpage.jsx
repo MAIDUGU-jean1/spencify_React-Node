289ee886a7a3a2ec0fa2ae779f7d25219d800a05
Spencer bright <spenzerbrightest@gmail.com>
2026-03-09 20:15:26 +0100

    changes made

--- src/Pages/Cartpage.jsx
+++ src/Pages/Cartpage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Cartpage = () => {
-  // Placeholder cart - you can add cart functionality later
-  const cartItems = []
+  
+  // const cartItems = []
 
   return (
 
@@ -13,3 +13,5 @@ const Cartpage = () => {
 
   )
 }
+
+export default Cartpage
